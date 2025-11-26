7b02d45d9a4b5561830661208b2ceb625bb37ca9
Mark Raynsford <code@io7m.com>
2025-11-26 16:28:27 +0000

    Enable more devices.

--- src/main/java/org/thepalaceproject/ait/AppiumDeviceConfigurations.java
+++ src/main/java/org/thepalaceproject/ait/AppiumDeviceConfigurations.java
@@ -13,12 +13,30 @@ public final class AppiumDeviceConfigurations
 
   }
 
+  /**
+   * @return The list of devices used for tests
+   *
+   * @see "https://www.browserstack.com/list-of-browsers-and-platforms/app_automate"
+   */
+
   public static Stream<AppiumDeviceConfiguration> deviceConfigurations()
   {
     return Stream.of(
       new AppiumDeviceConfiguration(
-        "Samsung Galaxy S22",
-        "12.0"
+        "Samsung Galaxy S23",
+        "13.0"
+      ),
+      new AppiumDeviceConfiguration(
+        "Pixel 9",
+        "16.0"
+      ),
+      new AppiumDeviceConfiguration(
+        "Moto G9 Play",
+        "10.0"
+      ),
+      new AppiumDeviceConfiguration(
+        "Galaxy Tab S11",
+        "16.0"
       )
     );
   }
