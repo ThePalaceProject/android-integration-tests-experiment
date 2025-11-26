b28d34614569bf85cf7a6bf4d6ccbb247e6a98a0
Mark Raynsford <code@io7m.com>
2025-11-26 13:15:22 +0000

    Use github token.

--- run-ci.sh
+++ run-ci.sh
@@ -1,6 +1,18 @@
 #!/bin/sh -ex
 
-git clone https://github.com/ThePalaceProject/mobile-certificates .credentials
+fatal()
+{
+  echo "run-ci.sh: fatal: $1" 1>&2
+  exit 1
+}
+
+if [ -z "${CI_GITHUB_ACCESS_TOKEN}" ]
+then
+  fatal "CI_GITHUB_ACCESS_TOKEN is not defined"
+fi
+
+git clone "https://${CI_GITHUB_ACCESS_TOKEN}:github.com/ThePalaceProject/mobile-certificates" .credentials
 cp .credentials/BrowserStack/PalaceAndroidTests/browserstack.yml .
 rm -rfv .credentials
+
 ./run.sh
